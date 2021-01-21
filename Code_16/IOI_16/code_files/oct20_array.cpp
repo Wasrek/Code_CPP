08a2c98b8a9cc71a26e11efe38e9ace31c3e42e3
Wasrek <w.wichada26@gmail.com>
2021-01-22 00:22:59 +0700

    push

--- Code_16/IOI_16/code_files/oct20_array.cpp
+++ Code_16/IOI_16/code_files/oct20_array.cpp
@@ -6,25 +6,27 @@
 */
 #include<bits/stdc++.h>
 using namespace std;
-#define N 200100
+#define N 1 << 18
 struct A{
-    long long opr,a,b,v;
+    int opr,a,b,v;
 };
-long long seg[N*4],lz[N*4];
+long long seg[N << 1],lz[N << 1];
 queue< A > q;
-vector< long long > g;
-map<long long,long long> mp,re;
-void lzpush(long long l,long long r,long long now){
+set< int > g;
+vector<int> gg;
+//map<int,int> mp,re;
+int re[N << 1];
+void lzpush(int l,int r,int now){
     if(lz[now]==1ll<<30) return ;
     if(l!=r){
         lz[now*2ll]=lz[now*2ll+1ll]=lz[now];
     }
-    seg[now]=(re[r]-re[l]+1ll)*lz[now];
+    seg[now]=(long long)(re[r+1]-re[l])*lz[now];
     // printf("%d %d %d %d %d\n",l,r,re[l],re[r],seg[now]);
     lz[now]=1ll<<30;
     return ;
 }
-void update(long long l,long long r,long long a,long long b,long long v,long long now){
+void update(int l,int r,int a,int b,long long v,int now){
     lzpush(l,r,now);
     if(l>b || r<a) return ;
     if(l>=a && r<=b){
@@ -32,54 +34,64 @@ void update(long long l,long long r,long long a,long long b,long long v,long lon
         lzpush(l,r,now);
         return ;
     }
-    long long mid=(l+r)>>1ll;
+    int mid=(l+r)>>1ll;
     update(l,mid,a,b,v,now*2ll);
     update(mid+1ll,r,a,b,v,now*2ll+1ll);
     seg[now]=seg[now*2ll]+seg[now*2ll+1ll];
     return ;
 }
-long long query(long long l,long long r,long long a,long long b,long long now){
+long long query(int l,int r,int a,int b,int now){
     lzpush(l,r,now);
     if(l>b || r<a) return 0ll;
     if(l>=a && r<=b){
         return seg[now];
     }
-    long long mid=(l+r)>>1ll;
+    int mid=(l+r)>>1ll;
     return query(l,mid,a,b,now*2ll)+query(mid+1,r,a,b,now*2ll+1ll);
 }
 int main()
 {
-    long long n,k,opr,a,b,v;
-    scanf("%lld %lld",&n,&k);
+    int n,k,opr,a,b,v;
+    scanf("%d %d",&n,&k);
     while(k--){
-        scanf("%lld",&opr);
+        scanf("%d",&opr);
         if(!opr){
-            scanf("%lld %lld %lld",&a,&b,&v);
-            b--;
+            scanf("%d %d %d",&a,&b,&v);
+//            b--;
             q.push({opr,a,b,v});
         }else{
-            scanf("%lld %lld",&a,&b);
-            b--;
+            scanf("%d %d",&a,&b);
+//            b--;
             q.push({opr,a,b,0});
         }
-        if(!mp[a])g.push_back(a),mp[a]=1;
-        if(!mp[b])g.push_back(b),mp[b]=1;
+        g.insert(a),g.insert(b);
+        //if(!mp[a])g.push_back(a),mp[a]=1;
+        //if(!mp[b])g.push_back(b),mp[b]=1;
     }
-    sort(g.begin(),g.end());
+    //if(!mp[0]) g.push_back(0);
+    //if(!mp[n]) g.push_back(n);
+    g.insert(0),g.insert(n);
+    //sort(g.begin(),g.end());
     int cou=0;
     for(auto x: g){
-        mp[x]=++cou;
-        re[cou]=x;
-        // printf("%d %d\n",x,cou);
+      //  mp[x]=++cou;
+      gg.push_back(x);
+        re[++cou]=x;
+     //    printf("%d %d\n",x,cou);
     }
-    for(int i=1;i<=cou*4;i++) lz[i]=1<<30;
+    for(int i=1;i< N << 1;i++) lz[i]=1<<30;
     while(!q.empty()){
+        int itl = lower_bound(gg.begin(),gg.end(),q.front().a)-gg.begin();
+        int itr = lower_bound(gg.begin(),gg.end(),q.front().b)-gg.begin();
+        itl++;
         if(!q.front().opr){
-            update(1,cou,mp[q.front().a],mp[q.front().b],q.front().v,1);
+            //cout << mp[q.front().a] << ' ' << mp[q.front().b]-1 << endl;
+
+            update(1,cou,itl,itr,q.front().v,1);
             // printf("upd %d %d %d\n",mp[q.front().a],mp[q.front().b],q.front().v);
             // printf("%lld\n",seg[1]);
         }else{
-            printf("%lld\n",query(1,cou,mp[q.front().a],mp[q.front().b],1));
+            printf("%lld\n",query(1,cou,itl,itr,1));
         }
         q.pop();
     }
